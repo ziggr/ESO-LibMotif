d7ed55e054c43424789d557bef4ca68f2abb7020
Zig Zichterman <github@ziggr.com>
2021-04-13 11:48:22 -0700

    whitespace only

--- LibMotif.lua
+++ LibMotif.lua
@@ -14,44 +14,44 @@ LibMotif.default = {}
 -- Scan ----------------------------------------------------------------------
 
 function LibMotif:ScanMotifs()
-	local motif        = {}
-	local max_motif_id 	= GetHighestItemStyleId()
-	local motif_seen_ct = 0
-	for motif_id = 1, max_motif_id do
-		motif_name = GetItemStyleName(motif_id)
-		if motif_name and motif_name ~= "" then
-			motif[motif_id] = { ["name"] = motif_name }
-			motif_seen_ct   = motif_seen_ct + 1
-		end
-	end
-	return { ["motif"        ] = motif
-	       , ["motif_seen_ct"] = motif_seen_ct
-	       , ["max_motif_id" ] = max_motif_id
-	       }
+    local motif        = {}
+    local max_motif_id  = GetHighestItemStyleId()
+    local motif_seen_ct = 0
+    for motif_id = 1, max_motif_id do
+        motif_name = GetItemStyleName(motif_id)
+        if motif_name and motif_name ~= "" then
+            motif[motif_id] = { ["name"] = motif_name }
+            motif_seen_ct   = motif_seen_ct + 1
+        end
+    end
+    return { ["motif"        ] = motif
+           , ["motif_seen_ct"] = motif_seen_ct
+           , ["max_motif_id" ] = max_motif_id
+           }
 end
 
 -- SlashCommand --------------------------------------------------------------
 
 function LibMotif.Scan()
-	local self = LibMotif
-	self.log:Debug("scan")
+    local self = LibMotif
+    self.log:Debug("scan")
 
-	local r = self:ScanMotifs()
-	self.motif 			= r.motif
-	self.motif_seen_ct 	= r.motif_seen_ct
-	self.max_motif_id 	= r.max_motif_id
+    local r = self:ScanMotifs()
+    self.motif          = r.motif
+    self.motif_seen_ct  = r.motif_seen_ct
+    self.max_motif_id   = r.max_motif_id
 end
 
 function LibMotif.SlashCommand(arg1)
-	self = LibMotif
+    self = LibMotif
     if arg1:lower() == "scan" then
-    	self.log:Info("Scanning...")
+        self.log:Info("Scanning...")
         LibMotif.Scan()
-    	self.log:Info(
-    		  "Scan complete. Motifs seen: %d. Highest id: %d"
-    		, self.motif_seen_ct
-    		, self.max_motif_id
-    		)
+        self.log:Info(
+              "Scan complete. Motifs seen: %d. Highest id: %d"
+            , self.motif_seen_ct
+            , self.max_motif_id
+            )
     end
 end
 
@@ -77,32 +77,32 @@ end
 -- Log -----------------------------------------------------------------------
 
 function LibMotif:InitLog()
-	if LibDebugLogger then
-		self.log = LibDebugLogger.Create(self.name)
-	end
-	if not self.log then
-		local NOP = {}
-		function NOP:Debug(...) end
-		function NOP:Info(...) end
-		function NOP:Warn(...) end
-		function NOP:Error(...) end
-		self.log = NOP
-	end
+    if LibDebugLogger then
+        self.log = LibDebugLogger.Create(self.name)
+    end
+    if not self.log then
+        local NOP = {}
+        function NOP:Debug(...) end
+        function NOP:Info(...) end
+        function NOP:Warn(...) end
+        function NOP:Error(...) end
+        self.log = NOP
+    end
 end
 
 -- Init ----------------------------------------------------------------------
 
 function LibMotif.OnAddOnLoaded(event, addonName)
-	self = LibMotif
+    self = LibMotif
     if addonName == LibMotif.name then
         if not LibMotif.name then return end
         LibMotif:Initialize()
-	    EVENT_MANAGER:UnregisterForEvent(self.name, EVENT_ADD_ON_LOADED)
+        EVENT_MANAGER:UnregisterForEvent(self.name, EVENT_ADD_ON_LOADED)
     end
 end
 
 function LibMotif:Initialize()
-	LibMotif:InitLog()
+    LibMotif:InitLog()
     self.saved_vars = ZO_SavedVars:NewAccountWide(
                               "LibMotifVars"
                             , self.saved_var_version
